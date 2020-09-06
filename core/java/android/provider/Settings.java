55a1eb338d0ffe7e2276a4e83390ea8e4f3f02a2
cjybyjk <cjybyjk@zjnu.edu.cn>
2020-09-07 01:44:05 +0800

    base: make app volume rows optional [1/2]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I5c02019a7cace127da41d72b386ad6f512871600

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6319,6 +6319,12 @@ public final class Settings {
         public static final String OOS_DISMISS_ALL_BUTTON = "oos_dismiss_all_button";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
